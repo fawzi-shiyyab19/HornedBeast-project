4aa4698bfefa7824316932c2e8c518a0a1a98db3
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-07-26 18:34:45 +0300

    branch02

--- src/components/HornedBeast .js
+++ src/components/HornedBeast .js
@@ -1,13 +1,32 @@
 import React from "react";
-import Cards from "./card";
-class HornedBeast extends React.Component{
-    render() {
-        return(
-            <>
-               <Cards title={this.props.title} image_url={this.props.imgUrl}  description={this.props.description}/>
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-            </> 
-            )
+
+class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          count: 0,
+        };
+      }
+    
+      handleClick = () => {
+        this.setState({
+          count: this.state.count + 1,
+        });
+    }
+    render() {
+        return (
+            <Card style={{ width: '18rem', display: 'inline-block' , margin:'1rem'}}>
+                <Card.Img variant="top" src={this.props.imgUrl} style={{ hieght:'10vh' }} onClick={this.handleClick} />
+                <Card.Body>
+                <Card.Title>{this.props.title} ❤️ {this.state.count}</Card.Title>
+                <Card.Text>{this.props.description}</Card.Text>
+                </Card.Body>
+            </Card>
+        );
     }
 }
+
 export default HornedBeast;
